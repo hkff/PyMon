1bcb4ed65333b401214252f3f53a81dcec423872
hkff <infigmo@gmail.com>
2016-01-09 11:46:45 +0100

    Minors

--- pymon.py
+++ pymon.py
@@ -17,3 +17,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 __author__ = 'walid'
 
+
+from fotl.fotlmon import *
+
+
+class meta(object):
+    """
+    Meta class
+    """
+    def __init__(self, formula=None):
+        """
+        If there are decorator arguments, the function
+        to be decorated is not passed to the constructor!
+        """
+        self.formula = formula
+        self.mon = Fotlmon(self.formula, Trace())
+
+    def __call__(self, f):
+        """
+        If there are decorator arguments, __call__() is only called
+        once, as part of the decoration process! You can only give
+        it a single argument, which is the function object.
+        """
+        def wrapped(*args):
+            # print("== Before calling %s%s" % (f.__name__, args))
+            # print(" Decorator arguments:", self.formula)
+            f(*args)
+            # print("== After calling %s%s" % (f.__name__, args))
+
+            args2 = ["'"+str(args[0].__class__.__name__)+"'"] + ["'"+str(x)+"'" for x in args[1:]]
+            args2 = ",".join(args2)
+            e = "%s(%s)" % (f.__name__, args2)
+            self.mon.trace.push_event(Event.parse('{'+e+'}'))
+            res = self.mon.monitor(once=True)
+            print(res)
+        return wrapped
+
+
+class Foo:
+    @meta(G(P("hello('Foo', '4', x)")))
+    def hello(b, a=None, c=None):
+        print("Hello world !")
+
+f = Foo()
+f.hello(4)
+f.hello(5)
+f.hello(4)
